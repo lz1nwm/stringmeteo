04070de2790f36519304e998a908d9d601aaeaba
SP <pachedjiev@gmail.com>
2025-07-21 09:32:50 +0300

    lemon

--- daily_chart.R
+++ daily_chart.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(scales)
 library(lubridate)
 library(openxlsx2)
+library(lemon)
 
 load('RData/bg_nimh.RData')
 
@@ -68,7 +69,7 @@ ggplot(data = dt.nimh.s[STATION_ID %in% stations]) +
     scale_x_date(date_breaks = '1 month', expand = expansion(0),
                  labels = scales::label_date_short())+
     scale_color_manual(values = colors, name = '')+
-    lemon::facet_rep_wrap(. ~ paste0(STATION_NAME,' (',STATION_ID,'), ', max(dt.nimh.s[ddate != Sys.Date(), ddate])), 
+    facet_rep_wrap(. ~ paste0(STATION_NAME,' (',STATION_ID,'), ', max(dt.nimh.s[ddate != Sys.Date(), ddate])), 
                scales = 'free_y',
                repeat.tick.labels = T,
                ncol = 2)+
@@ -118,7 +119,7 @@ ggplot(dt.nimh.w[STATION_ID %in% stations & ddate2 %between% week_dates]) +
                  labels = scales::label_date_short())+
     scale_color_manual(values = colors, name = '')+
     #facet_wrap(. ~ paste0(STATION_NAME,' (',STATION_ID,'), ',max(dt.nimh$ddate)), scales = 'free_y')+
-    lemon::facet_rep_wrap(. ~ paste0(STATION_NAME,' (',STATION_ID,'), ', max(dt.nimh$ddate)), 
+    facet_rep_wrap(. ~ paste0(STATION_NAME,' (',STATION_ID,'), ', max(dt.nimh$ddate)), 
                           scales = 'free_y',
                           repeat.tick.labels = T,
                           ncol = 2)+
