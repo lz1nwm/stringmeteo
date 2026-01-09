a309748574269341a59cbe1e82ceec7e699f49ba
lz1nwm <lz1nwm@users.noreply.github.com>
2026-01-09 23:04:31 +0200

    Update install.R

--- .devcontainer/install.R
+++ .devcontainer/install.R
@@ -4,7 +4,7 @@
 # Add lines here for installing packages:
 
 install.packages(
-    c("data.table", "ggplot2", "scales", "lubridate", "openxlsx2","lemon")
+    c("data.table", "ggplot2", "scales", "lubridate", "openxlsx2","lemon","httr")
 )
 
 # For example:
